38692bb9a6944774791b66581f9075d5591d547b
Leonidas <leonidas.ramirez.q@gmail.com>
2021-02-28 18:04:34 -0500

    Agregar archivos de proyecto.

---
+++ Pages/Maestro.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Pages
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Maestro : ContentPage
+    {
+        public Maestro()
+        {
+            InitializeComponent();
+        }
+
+        private void IrPagina1(object sender, EventArgs e)
+        {
+            App.MasterD.IsPresented = false;
+            App.MasterD.Detail.Navigation.PushAsync(new Pagina1());
+        }
+
+        private void IrPagina2(object sender, EventArgs e)
+        {
+            App.MasterD.IsPresented = false;
+            App.MasterD.Detail.Navigation.PushAsync(new Pagina2());
+        }
+    }
+}
---
+++ Pages/MainPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Pages
+{
+    public partial class MainPage : MasterDetailPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            this.Master = new Maestro();
+            this.Detail = new NavigationPage(new Detalle());
+
+            App.MasterD = this;
+        }
+    }
+}
